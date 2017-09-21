a00b1ff6b8cf213edf9b3775a83670c08789d9f9
Adam Barnhard <admbarn@gmail.com>
2017-09-20 21:46:33 -0500

    removed _ resolver

--- build/webpack.dev.conf.js
+++ build/webpack.dev.conf.js
@@ -18,9 +18,9 @@ module.exports = merge(baseWebpackConfig, {
   // cheap-module-eval-source-map is faster for development
   devtool: '#cheap-module-eval-source-map',
   plugins: [
-    new webpack.ProvidePlugin({
-      _: 'underscore',
-    }),
+    // new webpack.ProvidePlugin({
+    //   _: 'underscore',
+    // }),
     new webpack.DefinePlugin({
       'process.env': config.dev.env
     }),
